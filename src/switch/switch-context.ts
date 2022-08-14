b31d11dedd9c19ef459bd069b735ec310f2c320e
Roy Stewart <roythomasstewart@gmail.com>
2022-08-14 00:18:28 -0400

    Corrected Switch component build errors

--- src/switch/switch-context.ts
+++ src/switch/switch-context.ts
@@ -1,6 +1,11 @@
 import React from "react";
 
+export interface SwitchContextValue {
+    value: any,
+    isCaseSatisfied: boolean,
+}
+
 export const SwitchContext = React.createContext({
     value: null,
     isCaseSatisfied: false,
-});
+} as SwitchContextValue);
--- src/switch/switch.tsx
+++ src/switch/switch.tsx
@@ -1,9 +1,17 @@
+import React from "react";
 import { SwitchContext } from "./switch-context";
+
+interface SwitchProps {
+    children: any
+}
 
 export const Switch = ({
     children
-}) => {
-    const switchContext = {};
+}: SwitchProps) => {
+    const switchContext = {
+        value: null,
+        isCaseSatisfied: false,
+    };
     return <SwitchContext.Provider value={switchContext}>
         {children}
     </SwitchContext.Provider>;
