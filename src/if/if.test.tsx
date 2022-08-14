0777d9d84a893d4103df1ab5b1c0b824de776800
Roy Stewart <roythomasstewart@gmail.com>
2022-08-14 11:18:06 -0400

    Fixed formatting

--- src/if/if.test.tsx
+++ src/if/if.test.tsx
@@ -3,17 +3,13 @@ import { render, screen } from "@testing-library/react";
 import { If } from "./if";
 
 describe("If", () => {
-    it("should show its children when the condition is true", () => {
-        render(<If condition={true}>
-            Test text
-        </If>);
-        expect(screen.getByText("Test text")).toBeVisible();
-    });
+  it("should show its children when the condition is true", () => {
+    render(<If condition={true}>Test text</If>);
+    expect(screen.getByText("Test text")).toBeVisible();
+  });
 
-    it("should not show its children when the condition is false", () => {
-        render(<If condition={false}>
-            Test text
-        </If>);
-        expect(screen.queryByText("Test text")).toBeNull();
-    });
-})
+  it("should not show its children when the condition is false", () => {
+    render(<If condition={false}>Test text</If>);
+    expect(screen.queryByText("Test text")).toBeNull();
+  });
+});
