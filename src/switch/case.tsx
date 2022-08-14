56dda1e0a4e6cdbbe72d3b79dbefa6696ce6e511
Roy Stewart <roythomasstewart@gmail.com>
2022-08-14 00:20:47 -0400

    Corrected case element build errors

--- src/switch/case.tsx
+++ src/switch/case.tsx
@@ -1,10 +1,15 @@
 import { ReactComponentElement, useContext } from "react";
 import { SwitchContext } from "./switch-context";
 
+interface CaseProps {
+    value: any,
+    children: any,
+}
+
 export const Case = ({
     value,
     children,
-}) => {
+}: CaseProps) => {
     const { value: switchValue } = useContext(SwitchContext);
     
     if (switchValue === value) {
@@ -14,9 +19,13 @@ export const Case = ({
     }
 };
 
+interface DefaultCaseProps {
+    children: any,
+}
+
 export const DefaultCase = ({
     children,
-}) => {
+}: DefaultCaseProps) => {
     const { isCaseSatisfied } = useContext(SwitchContext);
     if (isCaseSatisfied) {
         return children;
