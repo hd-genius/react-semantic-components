5192df680240283121496a0909502aff64d29610
Roy Stewart <roythomasstewart@gmail.com>
2022-12-17 19:27:45 -0500

    Enabled comment docs

--- src/switch/default-case.tsx
+++ src/switch/default-case.tsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
 import { SwitchContext } from "./switch-context";
+import PropTypes from "prop-types";
 
 interface DefaultCaseProps {
     children: any;
 }
 
+/**
+ * The case that is shown when no other case is used
+ */
 export const DefaultCase = ({ children }: DefaultCaseProps) => {
     const { satisfiedCases } = useContext(SwitchContext);
     const isCaseSatisfied = satisfiedCases.length > 0;
     return isCaseSatisfied ? null : children;
+};
+
+DefaultCase.propTypes = {
+    /** the content to show */
+    children: PropTypes.any,
 };
