46f0ff7f7c287a76494e71dc0cad6d14fe8b1b41
Roy Stewart <roythomasstewart@gmail.com>
2022-12-17 20:25:33 -0500

    Added doc comments for If

--- src/if/if.tsx
+++ src/if/if.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 interface IfProps {
+  /** whether or not the contents should be shown */
   condition: boolean;
+  /** the content to show */
   children: any;
 }
 
+/**
+ * A component that conditionally shows or hides its content
+ */
 export const If = ({ condition, children }: IfProps) => {
   return condition ? <>{children}</> : null;
 };
