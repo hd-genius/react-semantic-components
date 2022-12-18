732529edf4b1dab0a6b709864a55ca3dd3409553
Roy Stewart <roythomasstewart@gmail.com>
2022-12-17 20:49:11 -0500

    Made peer deps external

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 // import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -17,6 +18,7 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
