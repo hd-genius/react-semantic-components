2dd5c68a0b92bda98f21dad8098bbfb9aa322ecc
Roy Stewart <roythomasstewart@gmail.com>
2022-12-17 21:05:44 -0500

    Added placeholder tests for Trycatch

---
+++ src/try-catch/try-catch.test.tsx
@@ -0,0 +1,9 @@
+describe("TryCatch", () => {
+    it.todo("should show the Try content when no error is thrown");
+
+    it.todo("should not show the Try content when an error is thrown");
+
+    it.todo("should show the catch content when an error is thrown");
+
+    it.todo("should not show the catch content when an error is not thrown");
+});
