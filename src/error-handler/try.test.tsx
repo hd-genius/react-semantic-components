bf0d1bfeeca21773e79881afe9080b92f50ba072
Roy Stewart <roythomasstewart@gmail.com>
2022-12-19 16:44:49 -0500

    Fixed formatting

--- src/error-handler/try.test.tsx
+++ src/error-handler/try.test.tsx
@@ -3,24 +3,24 @@ import { render, screen } from "@testing-library/react";
 import { Try } from "./try";
 
 describe("Try", () => {
-    const ComponentWithError = () => {
-        throw new Error("some error");
-    };
+  const ComponentWithError = () => {
+    throw new Error("some error");
+  };
 
-    it("should show it's content when no error is thrown", () => {
-        const textContent = "Some text";
-        render(<Try>{textContent}</Try>);
-        expect(screen.getByText(textContent)).toBeVisible();
-    });
+  it("should show it's content when no error is thrown", () => {
+    const textContent = "Some text";
+    render(<Try>{textContent}</Try>);
+    expect(screen.getByText(textContent)).toBeVisible();
+  });
 
-    it("should not show it's content when an error is thrown", () => {
-        const textContent = "Some text";
-        render(
-            <Try>
-                <ComponentWithError />
-                {textContent}
-            </Try>
-        );
-        expect(screen.queryByText(textContent)).toBeNull();
-    });
+  it("should not show it's content when an error is thrown", () => {
+    const textContent = "Some text";
+    render(
+      <Try>
+        <ComponentWithError />
+        {textContent}
+      </Try>
+    );
+    expect(screen.queryByText(textContent)).toBeNull();
+  });
 });
