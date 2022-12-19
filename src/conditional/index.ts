9cdb182289d213e76f969d8c7663d13aa7d78af2
Roy Stewart <roythomasstewart@gmail.com>
2022-12-19 18:32:47 -0500

    Added missing ElseIf export

--- src/conditional/index.ts
+++ src/conditional/index.ts
@@ -1,3 +1,4 @@
 export { If } from "./if";
 export { Conditional } from "./conditional";
+export { ElseIf } from "./else-if"
 export { Else } from "./else";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Switch, Case, DefaultCase } from "./switch";
 export { ForEach } from "./for-each";
-export { If, Conditional, Else } from "./conditional";
+export { If, Conditional, Else, ElseIf } from "./conditional";
 export { ErrorHandler, Try, Catch } from "./error-handler";
